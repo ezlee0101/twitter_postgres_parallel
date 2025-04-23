a7ca07812d4f635d688a67e5d78f2095664b7ac5
ezlee0101 <easterbuun@gmail..com>
2025-04-22 22:30:36 -0700

    pls

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -6,11 +6,9 @@ echo '==========================================================================
 echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
-    echo "→ Loading $file into pg_denormalized"
-    unzip -p "$file" \
-    | sed 's/\\u0000//g' \
-    | psql postgresql://postgres:pass@localhost:4115/postgres \
-        -c "COPY tweets_jsonb (data) FROM STDIN CSV QUOTE E'\\x01' DELIMITER E'\\x02';"
+    echo 
+    echo
+    unzip -p "$file" | sed 's/\\u0000//g' | psql postgres://postgres:pass@localhost:4535/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
     # copy your solution to the twitter_postgres assignment here
 done
 
@@ -18,11 +16,8 @@ echo '==========================================================================
 echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
-    echo "→ Loading $file into pg_normalized"
-    python3 load_tweets.py \
-    --db postgresql://postgres:pass@localhost:4116/postgres \
-    --inputs "$file" \
-    --print_every 10000
+    echo
+    python3 load_tweets.py --db postgresql://postgres:pass@localhost:4536/postgres --inputs "$file" --print_every 10000     
     # copy your solution to the twitter_postgres assignment here
 done
 
@@ -30,8 +25,6 @@ echo '==========================================================================
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    echo "→ Loading $file into pg_normalized_batch"
-    python3 -u load_tweets_batch.py \
-    --db postgresql://postgres:pass@localhost:4537/ \
-    --inputs "$file"
+    echo
+    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:4537/ --inputs $file 
 done
