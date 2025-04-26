48a9585e9c7dc876595d9f9ef87fd13b0e4d5fb2
ezlee0101 <easterbuun@gmail.com>
2025-04-25 17:13:56 -0700

    Update load_tweets_parallel.sh

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -10,9 +10,9 @@ time echo "$files" | parallel ./load_denormalized.sh
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time echo "$files" | parallel python3 load_tweets.py --db="postgresql://postgres:pass@localhost:1105" --inputs={}
+time echo "$files" | parallel python3 load_tweets.py --db="postgresql://postgres:pass@localhost:4536" --inputs={}
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:1106/ --inputs={}
+time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:4537/ --inputs={}
