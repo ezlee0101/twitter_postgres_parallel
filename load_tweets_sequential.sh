0e185b71d011dc9ca1ef48c66fa8c52db1375b6e
ezlee0101 <easterbuun@gmail.com>
2025-04-26 22:19:03 -0700

    Update load_tweets_sequential.sh

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -6,14 +6,15 @@ echo '==========================================================================
 echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
-    unzip -p "$file" | sed 's/\\u0000//g' | psql postgres://postgres:pass@localhost:4535/postgres -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+    echo
+    # copy your solution to the twitter_postgres assignment here
 done
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
-    python3 load_tweets.py --db postgresql://postgres:pass@localhost:4536/postgres --inputs "$file" --print_every 10000    
+    echo
     # copy your solution to the twitter_postgres assignment here
 done
 
@@ -21,5 +22,5 @@ echo '==========================================================================
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:4537/ --inputs $file
+    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs $file
 done
